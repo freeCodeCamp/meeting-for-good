8786449971c22cb836118c370862c0feb1acb7b7
Akira Laine <akira_laine@outlook.com>
2016-04-09 17:39:34 +1100

    initial commit

---
+++ app/views/index.jsx
@@ -0,0 +1,13 @@
+const React = require("react");
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Twitch</h1>
+            </div>
+        );
+    }
+}
+
+module.exports = Main;
