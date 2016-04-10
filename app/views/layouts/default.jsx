78e3dffcfbc8037b027065d3cf365c9b8f82c4a8
Aniruddh Agarwal <aaniruddh99@gmail.com>
2016-04-10 19:18:15 +0800

    Add material-ui and basic styles

--- app/views/layouts/default.jsx
+++ app/views/layouts/default.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
 export default class DefaultLayout extends React.Component {
-    render() {
-        return (
-            <html>
-                <head>
-                    <title>{this.props.title}</title>
-                    <link rel='stylesheet' type='text/css' href='/public/css/style.css' />
-                </head>
-                <body>{this.props.children}</body>
-            </html>
-        )
-    }
+  render() {
+    return (
+      <html>
+        <head>
+          <title>{this.props.title}</title>
+          <link rel="stylesheet" type="text/css" href="/public/css/style.css" />
+          <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,500" rel="stylesheet" type="text/css" />
+        </head>
+        <body>{this.props.children}</body>
+      </html>
+    );
+  }
 }
+
+DefaultLayout.propTypes = {
+  title: React.PropTypes.string,
+  children: React.PropTypes.element,
+};
