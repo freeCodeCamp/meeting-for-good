39a26b56fee71da057f13a019a0fae6398e962b5
Akira Laine <akira_laine@outlook.com>
2016-04-11 18:45:33 +1100

    made login page

--- app/views/layouts/default.jsx
+++ app/views/layouts/default.jsx
@@ -25,7 +25,7 @@ export default class DefaultLayout extends React.Component {
             {this.props.children}
           </main>
           <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-          <script type="text/javascript" src="js/materialize.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
         </body>
       </html>
     );
