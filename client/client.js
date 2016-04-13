178e2aac39856a47b3fb764a1fcbf793e99b26de
Aniruddh Agarwal <aaniruddh99@gmail.com>
2016-04-13 12:38:27 +0800

    Remove velocityjs from client

--- client/client.js
+++ client/client.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-import 'velocity-animate/velocity.min.js';
 import 'normalize.css/normalize.css';
 
 // Import App
