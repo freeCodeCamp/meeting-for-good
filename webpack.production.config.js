96fac896b4d44ac4f441155055409c700af94ed9
Aniruddh Agarwal <aaniruddh99@gmail.com>
2016-04-16 23:38:41 +0800

    Fix typo in webpack.prod.config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -39,7 +39,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        exclude: [/node_moduels/],
+        exclude: [/node_modules/],
         loaders: [
           'style',
           'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
