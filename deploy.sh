8700986eb37bbc539714212993f4636be6a6ccc7
Aniruddh Agarwal <awesomeaniruddh@users.noreply.github.com>
2016-04-19 21:49:40 +0800

    Prevent node_modules from getting deleted on deploy (#20)
    * Fix script deleting files from root
    
    * Remove debug log

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,6 @@ git merge development --no-edit # Merge in the master branch without prompting
 npm run build # Generate the bundled Javascript and CSS
 rm -r client
 rm -r server
-cp package.json build
-find . -maxdepth 1 -type f -exec rm {} \;
 cp -r build/* .
 rm -r build
 git add -A
