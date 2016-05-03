4c32d6d8fd37abdef1a59ebb121b0ecf1f0ae7b6
Akira Laine <akira_laine@outlook.com>
2016-05-03 17:08:49 +1100

    updated Navbar componenet w/ links

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -8,26 +8,41 @@ class Navbar extends React.Component {
     super();
     this.state = {
       userAvatar: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png',
+      user: false
     };
   }
 
   componentDidMount() {
     $.get('/api/auth/current', user => {
-      if (user !== undefined) {
-        const userAvatar = user.github ? user.github.avatar : user.facebook.avatar;
-        this.setState({ userAvatar });
+      if (user !== "") {
+        let userAvatar;
+        if(user.github) userAvatar = user.github.avatar;
+        else if(user.facebook) userAvatar = user.facebook.avatar;
+        this.setState({ userAvatar, user: true });
       }
     });
   }
 
   render() {
+    let links;
+    if(this.state.user){
+      links = ['<a id="user" href="/api/auth/logout">Logout</a>', '<a id="user" href="/dashboard">Dashboard</a>'];
+    } else {
+      links = []
+    }
     return (
-      <nav className="grey darken-4">
+      <nav className="grey darken-3">
         <div className="container">
           <a href="/" className="brand-logo">Lets Meet</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
-              <a href="#">
+              {links.forEach(el => {
+                $("#nav-mobile li a").not("#user").remove()
+                $("#nav-mobile li").prepend(el);
+              })}
+              <a href="/login">Login</a>
+              <a href="/signup">Signup</a>
+              <a href="#" id="user">
                 <img
                   styleName="nav-img"
                   src={this.state.userAvatar}
