29a6e212a37b1ea0524caa993523b5127c67765a
Aniruddh Agarwal <aaniruddh99@gmail.com>
2016-06-02 19:38:04 +0800

    Extend timerange limit from 23 to 24

--- client/pages/NewEvent.js
+++ client/pages/NewEvent.js
@@ -35,12 +35,12 @@ class NewEvent extends React.Component {
   componentDidMount() {
     const slider = document.getElementById('timeSlider');
     noUiSlider.create(slider, {
-      start: [0, 23],
+      start: [0, 24],
       connect: true,
       step: 1,
       range: {
         min: 0,
-        max: 23,
+        max: 24,
       }, format: wNumb({
         decimals: 0,
       }),
