de22d897aa875ee8aa8d798d27fae0450dc7a0c4
Akira Laine <akira_laine@outlook.com>
2016-06-30 17:28:32 +1100

    started work on home page

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
+import cssModules from 'react-css-modules';
+import styles from '../styles/home.css';
 
 import { isAuthenticated } from '../util/auth';
 
@@ -14,8 +16,21 @@ class Home extends React.Component {
   }
 
   render() {
-    return <div />;
+    return (
+      <div>
+        <header styleName="header">
+          <h2>LetsMeet</h2>
+          <hr />
+          <h6>The best meeting coordination app</h6>
+          <img src="https://raw.githubusercontent.com/AkiraLaine/LetsMeet/development/client/assets/dashboard-banner.jpg" alt="dashboard" />
+        </header>
+        <main>
+          <h2>Easy event creation</h2>
+          <h6>Creating an event is easy as saying 1, 2, 3.</h6>
+        </main>
+      </div>
+    );
   }
 }
 
-export default Home;
+export default cssModules(Home, styles);
