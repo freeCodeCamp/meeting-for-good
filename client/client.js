0e5f954b3a1dbecbd1a213ae1a965cab5c92a754
Aniruddh Agarwal <aaniruddh99@gmail.com>
2016-12-18 17:56:56 -0500

    Add react-redux provider to app

--- client/client.js
+++ client/client.js
@@ -1,31 +1,46 @@
 // Vendor Dependencies
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import { Router, Route, browserHistory, IndexRoute, Redirect } from 'react-router';
-import './styles/no-css-modules/nprogress.css';
-import './styles/no-css-modules/react-notifications.css';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import 'dialog-polyfill/dialog-polyfill.js';
 import 'dialog-polyfill/dialog-polyfill.css';
-
-require('es6-promise').polyfill();
+import './styles/no-css-modules/nprogress.css';
+import './styles/no-css-modules/react-notifications.css';
 
 // Import App
+import reducer from './reducers';
 import App from './components/App';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import EventDetails from './pages/EventDetails';
 import NewEvent from './pages/NewEvent';
 
+require('es6-promise').polyfill();
+
+const store = createStore(
+  combineReducers({
+    reducer,
+    routing: routerReducer,
+  }),
+);
+
+const history = syncHistoryWithStore(browserHistory, store);
+
 render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home} />
-      <Route path="dashboard" component={Dashboard} />
-      <Route path="event">
-        <Route path="new" component={NewEvent} />
-        <Route path=":uid" component={EventDetails} />
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+        <Route path="dashboard" component={Dashboard} />
+        <Route path="event">
+          <Route path="new" component={NewEvent} />
+          <Route path=":uid" component={EventDetails} />
+        </Route>
       </Route>
-    </Route>
-    <Redirect from="*" to="/" />
-  </Router>
+      <Redirect from="*" to="/" />
+    </Router>
+  </Provider>
 ), document.getElementById('app'));
