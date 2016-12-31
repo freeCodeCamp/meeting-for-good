b7f9d23c82bcf459db03b84dadb0c85536f2ac4c
Aniruddh Agarwal <aaniruddh99@gmail.com>
2016-12-31 20:48:55 +0800

    Make the Navbar component a function

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -6,57 +6,52 @@ import '../styles/no-css-modules/mdl.css';
 import facebookLogo from '../assets/facebook.png';
 import googleLogo from '../assets/google.png';
 
-class Navbar extends React.Component {
-  renderNav() {
-    if (this.props.user) {
-      return (
-        <div className="mdl-navigation">
-          <Link className="mdl-navigation__link" to="/dashboard">Dashboard</Link>
-          <a className="mdl-navigation__link" href="/api/auth/logout">Logout</a>
-          <a className="mdl-navigation__link" href="#avatar">
-            <img
-              alt="avatar"
-              styleName="nav-img"
-              src={this.props.userAvatar}
-            />
-          </a>
-        </div>
-      );
-    }
-
+const renderNav = (user, handleAuthClick, userAvatar) => {
+  if (user) {
     return (
-      <div className="mdl-navigation" styleName="login-btn">
-        <a
-          className="mdl-button mdl-js-button mdl-button--raised\
-                     mdl-js-ripple-effect mdl-color--indigo"
-          href="/api/auth/facebook"
-          onClick={this.props.handleAuthClick}
-        ><img src={facebookLogo} alt="Facebook Logo" /> Login with Facebook</a>
-        <a
-          className="mdl-button mdl-js-button mdl-button--raised\
-                     mdl-js-ripple-effect mdl-color--red"
-          href="/api/auth/google"
-          onClick={this.props.handleAuthClick}
-        ><img src={googleLogo} alt="Google Logo" /> Login with Google</a>
+      <div className="mdl-navigation">
+        <Link className="mdl-navigation__link" to="/dashboard">Dashboard</Link>
+        <a className="mdl-navigation__link" href="/api/auth/logout">Logout</a>
+        <a className="mdl-navigation__link" href="#avatar">
+          <img
+            alt="avatar"
+            styleName="nav-img"
+            src={userAvatar}
+          />
+        </a>
       </div>
     );
   }
+  return (
+    <div className="mdl-navigation" styleName="login-btn">
+      <a
+        className="mdl-button mdl-js-button mdl-button--raised\
+        mdl-js-ripple-effect mdl-color--indigo"
+        href="/api/auth/facebook"
+        onClick={handleAuthClick}
+      ><img src={facebookLogo} alt="Facebook Logo" /> Login with Facebook</a>
+      <a
+        className="mdl-button mdl-js-button mdl-button--raised\
+        mdl-js-ripple-effect mdl-color--red"
+        href="/api/auth/google"
+        onClick={handleAuthClick}
+      ><img src={googleLogo} alt="Google Logo" /> Login with Google</a>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <header className="mdl-layout__header">
-        <div className="mdl-layout__header-row">
-          <Link
-            to={this.props.conditionalHomeLink}
-            className="mdl-layout-title mdl-navigation__link"
-          >Lets Meet</Link>
-          <div className="mdl-layout-spacer" />
-          {this.renderNav()}
-        </div>
-      </header>
-    );
-  }
-}
+const Navbar = props => (
+  <header className="mdl-layout__header">
+    <div className="mdl-layout__header-row">
+      <Link
+        to={props.conditionalHomeLink}
+        className="mdl-layout-title mdl-navigation__link"
+      >Lets Meet</Link>
+      <div className="mdl-layout-spacer" />
+      {renderNav(props.user, props.handleAuthClick, props.userAvatar)}
+    </div>
+  </header>
+);
 
 Navbar.propTypes = {
   user: React.PropTypes.bool,
