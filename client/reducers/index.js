2a14286d490cddd0bcd4aa0751471890428278be
Aniruddh Agarwal <aaniruddh99@gmail.com>
2017-01-10 01:06:36 -0500

    Add reducers for entities and errors

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,21 @@
 import { combineReducers } from 'redux';
-import EventsReducer from './events';
-import UsersReducers from './users';
+import _ from 'lodash';
+
+const entities = (state = { events: [], currentUser: {} }, action) => {
+  if (action.response && action.response.entities) {
+    return _.merge({}, state, action.response.entities);
+  }
+
+  return state;
+};
+
+const errorMessages = (state = null, action) => {
+  const { error } = action;
+  if (error) return action.error;
+  return state;
+};
 
 export default combineReducers({
-  events: EventsReducer,
-  users: UsersReducers,
+  entities,
+  errorMessages,
 });
