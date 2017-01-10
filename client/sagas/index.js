fd19cd325b2d0f9f2e06b46f6dc7fa652d4845c3
Aniruddh Agarwal <aaniruddh99@gmail.com>
2017-01-10 01:04:27 -0500

    Move the sagas file

---
+++ client/sagas/index.js
@@ -0,0 +1,59 @@
+import { take, put, call, fork } from 'redux-saga/effects';
+import api from '../services';
+import * as actions from '../actions';
+
+const { events, event, user } = actions;
+
+function* fetchEntity(entity, apiFn, id) {
+  yield put(entity.request(id));
+  const { response, error } = yield call(apiFn, id);
+  if (response) yield put(entity.success(response));
+  else yield put(entity.failure(error));
+}
+
+export const fetchEvents = fetchEntity.bind(null, events, api.fetchEvents);
+export const fetchEvent  = fetchEntity.bind(null, event, api.fetchEvent);
+export const fetchUser   = fetchEntity.bind(null, user, api.fetchUser);
+
+function* loadEvents() {
+  yield call(fetchEvents);
+}
+
+function* loadEvent(id) {
+  yield call(fetchEvent, id);
+}
+
+function* loadUser() {
+  yield call(fetchUser);
+}
+
+// WATCHERS
+
+function* watchLoadEvents() {
+  while (true) {
+    yield take(actions.LOAD_EVENTS);
+    yield fork(loadEvents);
+  }
+}
+
+function* watchLoadEvent() {
+  while (true) {
+    const { id } = yield take(actions.LOAD_EVENT);
+    yield fork(loadEvent, id);
+  }
+}
+
+function* watchLoadUser() {
+  while (true) {
+    yield take(actions.FETCH_CURRENT_USER);
+    yield fork(loadUser);
+  }
+}
+
+export default function* root() {
+  yield [
+    fork(watchLoadEvents),
+    fork(watchLoadEvent),
+    fork(watchLoadUser),
+  ];
+}
