4cd3fe907423f463f1cdcc445ca36ddd28c98e2a
Aniruddh Agarwal <aaniruddh99@gmail.com>
2017-01-09 19:37:44 -0500

    Modify reducer

--- client/reducers/events.js
+++ client/reducers/events.js
@@ -1,1 +1,20 @@
-export default () => [];
+import {
+  EVENTS_FETCH_SUCCEEDED,
+} from '../actions/';
+
+
+export default (state = [], action) => {
+  switch (action) {
+    case EVENTS_FETCH_SUCCEEDED:
+      state = {
+        ...state,
+        events: action.payload,
+      };
+      break;
+
+    default:
+      break;
+  }
+
+  return state;
+};
