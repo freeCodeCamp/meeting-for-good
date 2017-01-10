86b8f81219933efff0caa5d1b8bba56a20a01ac5
Aniruddh Agarwal <aaniruddh99@gmail.com>
2017-01-09 21:21:08 -0500

    Complete API service

--- client/services/api.js
+++ client/services/api.js
@@ -1,14 +1,33 @@
 import 'isomorphic-fetch';
 import { checkStatus, parseJSON } from '../util/fetch.util';
-import { Schema, normalize } from 'normalize';
 
-const callApi = (endpoint, schema) => {
-  return fetch(endpoint)
+const fetchData = (endpoint) => {
+  return fetch(`/api/${endpoint}`, { credentials: 'same-origin' })
     .then(checkStatus)
     .then(parseJSON)
-    .then(json => normalize(json, schema))
-    .then(
-      response => ({ response }),
-      error => ({ error: error.message || 'Something bad happened.' }),
-    );
+    .then(response => ({ response }))
+    .catch(error => `Something bad happened: ${error}`);
 };
+
+const sendData = (endpoint, method, body) => {
+  return fetch(`/api/${endpoint}`, {
+    credentials: 'same-origin',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method,
+    body,
+  })
+    .then(checkStatus)
+    .then(parseJSON)
+    .then(response => ({ response }))
+    .catch(error => `Something bad happened: ${error}`);
+};
+
+export const fetchEvents = () => fetchData('/events/getByUser');
+export const fetchEvent = uid => fetchData(`/events/${uid}`);
+export const fetchUser = () => fetchData('/auth/current');
+export const newEvent = body => sendData('/events', 'POST', body);
+export const updateEvent = (id, method, body) =>
+  sendData(`/events/${id}`, method, body);
