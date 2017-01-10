e06951336d32729c0b74e28944faaf403f2953a1
Aniruddh Agarwal <aaniruddh99@gmail.com>
2017-01-10 01:03:48 -0500

    Make the fetch API work

--- client/services/api.js
+++ client/services/api.js
@@ -1,16 +1,28 @@
 import 'isomorphic-fetch';
+import { schema, normalize } from 'normalizr';
 import { checkStatus, parseJSON } from '../util/fetch.util';
 
-const fetchData = (endpoint) => {
-  return fetch(`/api/${endpoint}`, { credentials: 'same-origin' })
+const fetchData = (endpoint, schema) => {
+  return fetch(`/api${endpoint}`, { credentials: 'same-origin' })
     .then(checkStatus)
     .then(parseJSON)
-    .then(response => ({ response }))
-    .catch(error => `Something bad happened: ${error}`);
+    .then(json => Object.assign({}, normalize(json, schema)))
+    .then((json) => {
+      if (json.entities.events) {
+        json.entities.events = Object.keys(json.entities.events).map(key =>
+          json.entities.events[key],
+        );
+      }
+      return json;
+    })
+    .then(
+      response => ({ response }),
+      error => ({ error }),
+    );
 };
 
 const sendData = (endpoint, method, body) => {
-  return fetch(`/api/${endpoint}`, {
+  return fetch(`/api${endpoint}`, {
     credentials: 'same-origin',
     headers: {
       Accept: 'application/json',
@@ -21,12 +33,24 @@ const sendData = (endpoint, method, body) => {
   })
     .then(checkStatus)
     .then(parseJSON)
-    .then(response => ({ response }))
-    .catch(error => `Something bad happened: ${error}`);
+    .then(
+      response => ({ response }),
+      error => ({ error: error || 'Something Bad Happened :O' }),
+    );
 };
 
-export const fetchEvents = () => fetchData('/events/getByUser');
-export const fetchEvent = uid => fetchData(`/events/${uid}`);
-export const fetchUser = () => fetchData('/auth/current');
+const userSchema = new schema.Entity('currentUser', {}, {
+  idAttribute: '_id',
+});
+
+const eventSchema = new schema.Entity('events', {}, {
+  idAttribute: '_id',
+});
+
+const eventSchemaArray = new schema.Array(eventSchema);
+
+export const fetchEvents = () => fetchData('/events/getByUser', eventSchemaArray);
+export const fetchEvent = uid => fetchData(`/events/${uid}`, eventSchema);
+export const fetchUser = () => fetchData('/auth/current', userSchema);
 export const newEvent = body => sendData('/events', 'POST', body);
 export const updateEvent = (id, method, body) => sendData(`/events/${id}`, method, body);
