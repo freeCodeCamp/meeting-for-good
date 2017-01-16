d32f78c6614932412c722133c071e851dd730a22
Aniruddh Agarwal <aaniruddh99@gmail.com>
2017-01-15 20:47:30 -0500

    Abstract API functions to useFetch

--- client/services/api.js
+++ client/services/api.js
@@ -2,49 +2,48 @@ import 'isomorphic-fetch';
 import { schema, normalize } from 'normalizr';
 import { checkStatus, parseJSON } from '../util/fetch.util';
 
-const fetchData = (endpoint, schema) => {
-  return fetch(`/api${endpoint}`, { credentials: 'same-origin' })
+const fixJSON = (json) => {
+  if (json.entities.events) {
+    json.entities.events = Object.keys(json.entities.events).map(key =>
+      json.entities.events[key],
+    );
+  }
+
+  if (json.entities.currentUser) {
+    json.entities.currentUser = json.entities.currentUser[
+      Object.keys(json.entities.currentUser)[0]
+    ];
+  }
+
+  return json;
+};
+
+const useFetch = (endpoint, options, schema) => {
+  return fetch(`/api${endpoint}`, options)
     .then(checkStatus)
     .then(parseJSON)
     .then(json => Object.assign({}, normalize(json, schema)))
-    .then((json) => {
-      if (json.entities.events) {
-        json.entities.events = Object.keys(json.entities.events).map(key =>
-          json.entities.events[key],
-        );
-      }
-
-      if (json.entities.currentUser) {
-        json.entities.currentUser = json.entities.currentUser[
-          Object.keys(json.entities.currentUser)[0]
-        ];
-      }
-
-      return json;
-    })
+    .then(fixJSON)
     .then(
       response => ({ response }),
       error => ({ error }),
     );
 };
 
-const sendData = (endpoint, method, body) => {
-  return fetch(`/api${endpoint}`, {
-    credentials: 'same-origin',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    method,
-    body,
-  })
-    .then(checkStatus)
-    .then(parseJSON)
-    .then(
-      response => ({ response }),
-      error => ({ error: error || 'Something Bad Happened :O' }),
-    );
-};
+const fetchData = (endpoint, schema) => useFetch(endpoint, {
+  credentials: 'same-origin',
+  method: 'GET',
+}, schema);
+
+const sendData = (endpoint, method, body, schema) => useFetch(endpoint, {
+  credentials: 'same-origin',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  method,
+  body,
+}, schema);
 
 const userSchema = new schema.Entity('currentUser', {}, {
   idAttribute: '_id',
@@ -59,5 +58,5 @@ const eventSchemaArray = new schema.Array(eventSchema);
 export const fetchEvents = () => fetchData('/events/getByUser', eventSchemaArray);
 export const fetchEvent = uid => fetchData(`/events/${uid}`, eventSchema);
 export const fetchUser = () => fetchData('/auth/current', userSchema);
-export const newEvent = body => sendData('/events', 'POST', body);
-export const updateEvent = (id, method, body) => sendData(`/events/${id}`, method, body);
+export const newEvent = body => sendData('/events', 'POST', body, eventSchema);
+export const updateEvent = (id, method, body) => sendData(`/events/${id}`, method, body, eventSchema);
