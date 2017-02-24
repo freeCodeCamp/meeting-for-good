027ef207f077665a52f2e2c61a9708fea7eadd27
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-02-24 19:35:27 -0300

    corect the asunc method for user and notifications

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import cssModules from 'react-css-modules';
 import autobind from 'autobind-decorator';
+import fetch from 'isomorphic-fetch';
+import { checkStatus, parseJSON } from '../util/fetch.util';
 import styles from '../styles/navbar.css';
 import '../styles/no-css-modules/mdl.css';
+
 
 class Navbar extends Component {
   constructor(props) {
@@ -18,20 +21,12 @@ class Navbar extends Component {
     };
   }
 
-  componentWillMount() {
-    $.get('/api/auth/current', (user) => {
-      if (user) {
-        const userAvatar = user.avatar;
-        $.get('/api/events/getGuestNotifications', (notices) => {
-          if (notices) {
-            // this.setState({ notifications: notices });
-            this.setState({ userAvatar, user: true, curUser: user._id, conditionalHomeLink: '/Dashboard', notifications: notices });
-          } else {
-            this.setState({ userAvatar, user: true, curUser: user._id, conditionalHomeLink: '/Dashboard' });
-          }
-        });
-      }
-    });
+  async componentWillMount() {
+    await this.loadUser();
+  }
+
+  async componentDidMount() {
+    await this.loadNotifications();
   }
 
   @autobind
@@ -41,26 +36,77 @@ class Navbar extends Component {
     }
   }
 
+  async loadUser() {
+    const response = await fetch('/api/auth/current', { credentials: 'same-origin' });
+    let user;
+    try {
+      checkStatus(response);
+      user = await parseJSON(response);
+      return user;
+    } catch (err) {
+      console.log(err);
+      return;
+    } finally {
+      const userAvatar = user.avatar;
+      this.setState({ userAvatar, user: true, curUser: user._id, conditionalHomeLink: '/Dashboard' });
+    }
+  }
+
+  async loadNotifications() {
+    const response = await fetch('/api/events/getGuestNotifications', { credentials: 'same-origin' });
+    let notices;
+    try {
+      checkStatus(response);
+      notices = await parseJSON(response);
+      return notices;
+    } catch (err) {
+      console.log(err);
+      return;
+    } finally {
+      this.setState({ notifications: notices });
+    }
+  }
+
+  renderNotifications() {
+    const { notifications } = this.state;
+    return (
+      <div>
+        <button
+          style={(notifications) ? { color: 'red' } : { color: 'white' }}
+          id="menu-notifications"
+          className="mdl-button mdl-js-button mdl-button--icon"
+        >
+          <i className="material-icons">priority_high</i>
+        </button>
+        <ul className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" htmlFor="menu-notifications">
+          {notifications.map((event) => {
+            const participants = event.participants;
+            return participants.map((participant) => {
+              console.log(participant.userId.toString(), this.state.curUser.toString());
+              if (participant.userId.toString() !== this.state.curUser.toString()) {
+                return (
+                  <li className="mdl-menu__item mdl-menu__item--full-bleed-divider" key={participant._id} >
+                    <div>
+                      <span> {participant.name} accept your invite for </span>
+                      <Link to={`/event/${event._id}`} >{event.name}
+                      </Link>
+                      <button className="mdl-button"> Dismiss </button>
+                    </div>
+                  </li>);
+              }
+            });
+          })
+          }
+        </ul>
+      </div>
+    );
+  }
 
   renderNav() {
     if (this.state.user) {
       return (
         <div className="mdl-navigation">
-          <button id="menu-speed" className="mdl-button mdl-js-button mdl-button--icon">
-            <i className="material-icons">more_vert</i>
-          </button>
-          <ul className="mdl-menu mdl-js-menu mdl-js-ripple-effect" htmlFor="menu-speed">
-            {this.state.notifications.map((event) => {
-              const participants = event.participants;
-              return participants.map((participant) => {
-                if (participant.userId.toString() !== this.state.curUser.toString()) {
-                // console.log(participant.name, event.name);
-                  return <li className="mdl-menu__item" key={participant._id} > {participant.name} accept ypur invite for {event.name} </li>;
-                }
-              });
-            })
-            }
-          </ul>
+          {this.renderNotifications()}
           <Link className="mdl-navigation__link" to="/dashboard">Dashboard</Link>
           <a className="mdl-navigation__link" href="/api/auth/logout">Logout</a>
           <a className="mdl-navigation__link" href="#">
