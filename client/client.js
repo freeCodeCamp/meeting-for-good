9e739eaebb40e6f697abb8f9649d7e6be927bb73
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-03-02 09:33:02 -0300

    fix routes

--- client/client.js
+++ client/client.js
@@ -1,4 +1,40 @@
 import React, { Component } from 'react';
+import { Router, Route, browserHistory, IndexRoute, Redirect } from 'react-router';
+import 'dialog-polyfill/dialog-polyfill';
+import 'dialog-polyfill/dialog-polyfill.css';
+// Import App
+import App from './components/App';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import EventDetails from './pages/EventDetails';
+import NewEvent from './pages/NewEvent';
+
+// Vendor Dependencies
+import './styles/no-css-modules/nprogress.css';
+import './styles/no-css-modules/react-notifications.css';
+
+require('es6-promise').polyfill();
+
+export default class Client extends Component {
+
+  render() {
+    return (
+      <Router history={browserHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Home} />
+          <Route path="dashboard" component={Dashboard} />
+          <Route path="event">
+            <Route path="new" component={NewEvent} />
+            <Route path=":uid" component={EventDetails} />
+          </Route>
+        </Route>
+        <Redirect from="*" to="/" />
+      </Router>
+    );
+  }
+}
+
+/* import React, { Component } from 'react';
 import { Router, browserHistory, Redirect } from 'react-router';
 import 'dialog-polyfill/dialog-polyfill';
 import 'dialog-polyfill/dialog-polyfill.css';
@@ -54,4 +90,4 @@ export default class Client extends Component {
       </Router>
     );
   }
-}
+}*/
