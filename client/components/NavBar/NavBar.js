acab0b0a89a6f7af5f0b9ea66590cba162262ddb
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-03-16 17:29:33 -0300

    login buttons change fontSize

--- client/components/NavBar/NavBar.js
+++ client/components/NavBar/NavBar.js
@@ -67,12 +67,15 @@ class NavBar extends Component {
         fontSize: '15px',
         color: '#ffffff',
         margin: 0,
+        border: 0,
       },
       loginButton: {
         color: '#ffffff',
         primary: true,
-        fontSize: '25px',
+        fontSize: '22px',
         width: '150px',
+        backgroundColor: 'transparent',
+        boxShadow: '0px',
       },
       TollbarGroup: {
         paddingRight: '5%',
@@ -140,7 +143,7 @@ class NavBar extends Component {
         style={styles.TollbarGroup}
       >
 
-        <RaisedButton style={styles.loginButton} backgroundColor="#006400" onTouchTap={this.handleAuthClick}>
+        <RaisedButton style={styles.loginButton} backgroundColor="transparent" onTouchTap={this.handleAuthClick}>
           Login
         </RaisedButton>
       </ToolbarGroup>
