b801ae3ba63c2f6c315070207bb36f62362e43b3
Jean Philip de Rogatis <jrogatis@metaconexao.com.br>
2017-03-27 13:48:29 -0300

    Enhance home page layout (#179)
    * change dashbaord banner image
    
    * change from require to import images at home
    
    * change footer and header color and styles
    
    * change the banner image
    
    * new banner

--- client/pages/home/index.js
+++ client/pages/home/index.js
@@ -4,6 +4,10 @@ import cssModules from 'react-css-modules';
 
 import styles from './home.css';
 import { isAuthenticated } from '../../util/auth';
+import dashboardBanner from '../../assets/dashboard-banner.png';
+import mainBannerImage from '../../assets/main-banner.png';
+import enterAvailImage from '../../assets/enteravail.gif';
+import timeZonesImage from '../../assets/timezones.png';
 
 class Home extends React.Component {
   async componentWillMount() {
@@ -23,28 +27,28 @@ class Home extends React.Component {
           <hr />
           <h6>The best meeting coordination app</h6>
           <img
-            src={require('../../assets/dashboard-banner.png')}
+            src={dashboardBanner}
             alt="dashboard"
           />
         </header>
         <div styleName="content">
           <h3>Easy event creation</h3>
           <h6>Creating an event is easy as saying 1, 2, 3.</h6>
-          <img src={require('../../assets/main-banner.png')} alt="new event" />
+          <img src={mainBannerImage} alt="new event" />
           <hr styleName="hr" />
           <h3>Entering your availabily is a breeze</h3>
           <h6>Using our simplistic availabily grid, entering your availabily just got easier</h6>
-          <img src={require('../../assets/enteravail.gif')} styleName="gif" alt="enter availabily" />
+          <img src={enterAvailImage} styleName="gif" alt="enter availabily" />
           <hr styleName="hr" />
           <h3>Timezones supported</h3>
           <h6>Don&#39;t worry about having to get everyone to convert to your timezone. Let us take of it!</h6>
-          <img src={require('../../assets/timezones.png')} styleName="timezones" alt="timezone" />
+          <img src={timeZonesImage} styleName="timezones" alt="timezone" />
           <p><small>Same event. Image on the left: UTC+11. Image on the right UTC-5 (Daylight saving included)</small></p>
         </div>
         <footer>
           <div>
             <div styleName={'divider'} />
-            <p>Made with ❤ by &nbsp;
+            <p>Made with ❤ and lots of ☕ <br /> by &nbsp;
               <a href="https://github.com/AkiraLaine" target="_blank">Akira Laine</a>, &nbsp;
               <a href="https://github.com/awesomeaniruddh/" target="_blank">Aniruddh Agarwal</a>  and&nbsp;
               <a href="https://github.com/jrogatis/" target="_blank">Jean Philip de Rogatis</a>
