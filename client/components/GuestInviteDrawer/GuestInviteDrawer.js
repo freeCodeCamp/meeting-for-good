75144d58d46b59b9ac634696e7b924febae71aae
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-03-29 23:28:31 -0300

    delete uneceassry code

--- client/components/GuestInviteDrawer/GuestInviteDrawer.js
+++ client/components/GuestInviteDrawer/GuestInviteDrawer.js
@@ -242,7 +242,6 @@ class GuestInviteDrawer extends Component {
       searchText,
       snackbarMsg,
       linearProgressVisible,
-      guestsToDisplay,
     } = this.state;
 
     const fullUrl = `${location.protocol}//${location.hostname}${(location.port ? `:${location.port}` : '')}/event/${event._id}`;
