6b80e402740c66e22a6f0fe0beec6a14608900e7
Jean Philip de Rogatis <jrogatis@metaconexao.com.br>
2017-03-29 21:35:02 -0300

    Remove "Join Event" button (#195)
    * delete unused func
    
    * remove unused lib
    
    * delete weekdays reference
    
    * change logic at grid to add
    a new partipant
    
    * add notifivation if event updateted
    
    * add message to join event
    
    * clean unused code
    
    * back to 7.7.2
    
    * correct misspellings
    
    * clear blank line
    
    * correct text join event
    
    * correct snackbar
    
    * add more time to hide
    
    * correct font size
    
    * add line

--- client/components/EventDetailsComponent/EventDetailsComponent.js
+++ client/components/EventDetailsComponent/EventDetailsComponent.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import update from 'react-addons-update';
 import autobind from 'autobind-decorator';
 import cssModules from 'react-css-modules';
 import fetch from 'isomorphic-fetch';
-import jsonpatch from 'fast-json-patch';
 import { Card, CardTitle, CardText } from 'material-ui/Card';
-import RaisedButton from 'material-ui/RaisedButton';
+import Snackbar from 'material-ui/Snackbar';
 
 import DeleteModal from '../../components/DeleteModal/DeleteModal';
 import Notification from '../../components/vendor/react-notification';
@@ -44,6 +42,9 @@ class EventDetailsComponent extends React.Component {
       notificationTitle: '',
       showButtonAviability: 'none',
       showAvailabilityGrid: 'block',
+      isParticipant: true,
+      snackBarOpen: false,
+      snackBarMsg: '',
     };
   }
 
@@ -55,14 +56,13 @@ class EventDetailsComponent extends React.Component {
       let myAvailability = [];
 
       // find actual user particant record
-      const isParticipant = this.state.participants.find(participant =>
+      const isCurParticipant = this.state.participants.find(participant =>
         participant.userId === curUser._id,
       );
-
       // if curUser have aviability show heatMap
-      if (isParticipant) {
-        if (isParticipant.availability) {
-          myAvailability = isParticipant.availability;
+      if (isCurParticipant) {
+        if (isCurParticipant.availability) {
+          myAvailability = isCurParticipant.availability;
           if (myAvailability.length) {
             showHeatmap = true;
             showAvailabilityGrid = 'none';
@@ -70,7 +70,13 @@ class EventDetailsComponent extends React.Component {
         }
       } else {
         showHeatmap = false;
-        showAvailabilityGrid = 'none';
+        showAvailabilityGrid = 'block';
+        this.setState({
+          isParticipant: false,
+          snackBarOpen: true,
+          snackBarMsg: 'Please add your availability to join the event.',
+        });
+
       }
       this.setState({ showHeatmap, showAvailabilityGrid, myAvailability });
     }
@@ -80,42 +86,6 @@ class EventDetailsComponent extends React.Component {
     $('.notification-bar-action').on('click', () => {
       this.setState({ notificationIsActive: false });
     });
-  }
-
-  selectElementContents(el) {
-    let range;
-    if (window.getSelection && document.createRange) {
-      range = document.createRange();
-      const sel = window.getSelection();
-      range.selectNodeContents(el);
-      sel.removeAllRanges();
-      sel.addRange(range);
-    } else if (document.body && document.body.createTextRange) {
-      range = document.body.createTextRange();
-      range.moveToElementText(el);
-      range.select();
-    }
-  }
-
-  @autobind
-  async joinEvent() {
-    const { curUser } = this.props;
-    const { name, avatar, _id: userId } = curUser;
-    const participant = { name, avatar, userId };
-    const event = update(this.state.event, { $set: this.state.event });
-    const observerEvent = jsonpatch.observe(event);
-
-    event.participants.push(participant);
-
-    const eventParticipantsIds = update(this.state.eventParticipantsIds, {
-      $push: [curUser._id],
-    });
-    const patches = jsonpatch.generate(observerEvent);
-    const response = await this.props.cbEditEvent(patches, event._id);
-    if (response) {
-      this.sendEmailOwner(event);
-      this.setState({ event, eventParticipantsIds, showAvailabilityGrid: 'block' });
-    }
   }
 
   async loadOwnerData(_id) {
@@ -196,7 +166,16 @@ class EventDetailsComponent extends React.Component {
         const me = event.participants.find(participant =>
           participant.userId === curUser._id,
         );
-        this.setState({ showHeatmap: true, event, participants: event.participants, myAvailability: me.availability });
+        this.setState({
+          showHeatmap: true,
+          event,
+          participants: event.participants,
+          myAvailability: me.availability,
+          notificationIsActive: true,
+          notificationMessage: 'Saved availability successfully.',
+          notificationTitle: 'Success!',
+        });
+        return event;
       } catch (err) {
         console.log(err);
         this.setState({
@@ -206,12 +185,6 @@ class EventDetailsComponent extends React.Component {
         });
         return;
       }
-
-      this.setState({
-        notificationIsActive: true,
-        notificationMessage: 'Saved availability successfully.',
-        notificationTitle: 'Success!',
-      });
     }
   }
 
@@ -227,20 +200,36 @@ class EventDetailsComponent extends React.Component {
     this.props.cbDeleteEvent(event._id);
   }
 
+  @autobind
+  handleSnackBarRequestClose() {
+    this.setState({
+      snackBarOpen: false,
+    });
+  }
+
   render() {
     const {
       event,
       showHeatmap, participants,
-      myAvailability, eventParticipantsIds,
-      dates, showAvailabilityGrid } = this.state;
+      myAvailability,
+      dates, showAvailabilityGrid, snackBarOpen, snackBarMsg } = this.state;
     const { curUser } = this.props;
     const availability = participants.map(participant => participant.availability);
     let isOwner;
-
     // check if the curUser is owner
     if (curUser !== undefined) {
       isOwner = event.owner === curUser._id;
     }
+
+    const inLineStyles = {
+      snackBar: {
+        border: '5px solid #fffae6',
+        contentSyle: {
+          fontSize: '16px',
+          textAlign: 'center',
+        },
+      },
+    };
 
     const notifActions = [{
       text: 'Dismiss',
@@ -248,54 +237,46 @@ class EventDetailsComponent extends React.Component {
     }];
 
     return (
-      <Card styleName="card">
-        {isOwner ? <DeleteModal event={event} cbEventDelete={this.handleDelete} /> : null}
-        <CardTitle styleName="cardTitle">{event.name}</CardTitle>
-        <CardText>
-          <BestTimesDisplay event={event} disablePicker />
-          {(showHeatmap) ?
-            <div id="heatmap">
-              <AvailabilityGrid
-                dates={dates}
-                availability={availability}
-                editAvail={this.editAvail}
-                participants={participants}
-                heatmap
-              />
-            </div> :
-            <div id="grid" styleName="aviabilityContainer" >
-              <div style={{ display: showAvailabilityGrid }}>
-                <AvailabilityGrid
-                  event={event}
-                  dates={dates}
-                  user={curUser}
-                  availability={availability}
-                  myAvailability={myAvailability}
-                  submitAvail={this.submitAvailability}
-                  closeGrid={this.closeGrid}
-                />
-              </div>
-              {(Object.keys(curUser).length > 0) ?
-                (eventParticipantsIds.indexOf(curUser._id) > -1) ?
-                  null
-                  :
-                  <RaisedButton
-                    onClick={this.joinEvent}
-                    label={'Join Event'}
-                    backgroundColor="#006400"
-                    labelColor="#ffffff"
+      <div styleName="wrapper">
+        <div styleName="cardWrapper">
+          <Card styleName="card">
+            {isOwner ? <DeleteModal event={event} cbEventDelete={this.handleDelete} /> : null}
+            <CardTitle styleName="cardTitle">{event.name}</CardTitle>
+            <CardText>
+              <BestTimesDisplay event={event} disablePicker />
+              {(showHeatmap) ?
+                <div id="heatmap">
+                  <AvailabilityGrid
+                    dates={dates}
+                    availability={availability}
+                    editAvail={this.editAvail}
+                    participants={participants}
+                    heatmap
                   />
-                : null
-            }
-            </div>
-          }
-          <br />
-          <ParticipantsList
-            event={event}
-            curUser={curUser}
-            showInviteGuests={this.handleShowInviteGuestsDrawer}
-          />
-        </CardText>
+                </div> :
+                <div id="grid" styleName="aviabilityContainer" >
+                  <div style={{ display: showAvailabilityGrid }}>
+                    <AvailabilityGrid
+                      event={event}
+                      dates={dates}
+                      user={curUser}
+                      availability={availability}
+                      myAvailability={myAvailability}
+                      submitAvail={this.submitAvailability}
+                      closeGrid={this.closeGrid}
+                    />
+                  </div>
+                </div>
+              }
+              <br />
+              <ParticipantsList
+                event={event}
+                curUser={curUser}
+                showInviteGuests={this.handleShowInviteGuestsDrawer}
+              />
+            </CardText>
+          </Card>
+        </div>
         <Notification
           isActive={this.state.notificationIsActive}
           message={this.state.notificationMessage}
@@ -305,7 +286,18 @@ class EventDetailsComponent extends React.Component {
           dismissAfter={3000}
           activeClassName="notification-bar-is-active"
         />
-      </Card>
+        <Snackbar
+          style={inLineStyles.snackBar}
+          bodyStyle={{ height: 'flex' }}
+          contentStyle={inLineStyles.snackBar.contentSyle}
+          open={snackBarOpen}
+          message={snackBarMsg}
+          action="dismiss"
+          autoHideDuration={5000}
+          onRequestClose={this.handleSnackBarRequestClose}
+          onActionTouchTap={this.handleSnackBarRequestClose}
+        />
+      </div>
     );
   }
 }
