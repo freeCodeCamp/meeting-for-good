6785a769086c2f575a06648da1f5d2edc48dca03
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-05-14 22:49:26 -0300

    fix Display Dates

--- client/components/BestTimeDisplay/BestTimeDisplay.js
+++ client/components/BestTimeDisplay/BestTimeDisplay.js
@@ -39,18 +39,22 @@ class BestTimeDisplay extends Component {
       }
     });
     if (availability.length > 1) {
+      // need to find the participant with most availabilitys to be the base one;
+      availability.sort((a, b) => b.length - a.length);
       for (let i = 0; i < availability[0].length; i += 1) {
         const current = availability[0][i];
         let count = 0;
         for (let j = 0; j < availability.length; j += 1) {
           for (let k = 0; k < availability[j].length; k += 1) {
-            if (moment(availability[j][k][0]).format('D M YYYY HH MM').toString()
-              === moment(current[0]).format('D M YYYY HH MM').toString()) {
+            if (moment(availability[j][k][0]).format('D M YYYY HH mm').toString()
+              === moment(current[0]).format('D M YYYY HH mm').toString()) {
               count += 1;
             }
           }
         }
-        if (count === availability.length) overlaps.push(current);
+        if (count === availability.length) {
+          overlaps.push(current);
+        }
       }
       if (overlaps.length !== 0) {
         let index = 0;
