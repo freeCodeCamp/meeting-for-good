c9e705f96b1509cee2ba998a2fe8d53562f27d57
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-05-18 09:48:06 -0300

    fix conflicts

--- client/components/CellGrid/CellGrid.js
+++ client/components/CellGrid/CellGrid.js
@@ -1,65 +1,129 @@
 import React, { Component } from 'react';
 import cssModules from 'react-css-modules';
 import PropTypes from 'prop-types';
+import moment from 'moment';
+import _ from 'lodash';
 
-import { getMinutes } from '../../util/time-format';
 import styles from './cell-grid.css';
 
 class CellGrid extends Component {
 
-  // Change the border of the cell if it's minutes = 0 or 30 to help visually
-  // separate 15 minute blocks from 30 minute and 1 hour blocks.
-  static formatCellBorder(time) {
-    const minutes = getMinutes(time);
+  static styleNameCompose(
+    heightlightedUser, heatMapMode, participants, backgroundColors, curUser, time) {
+    // select the class for the border base style
+    let style = 'cell';
+    const minutes = time.minutes();
     if (minutes === 0) {
-      return { borderLeft: '1px solid rgb(120, 120, 120)' };
+      style += ' cellBorderHour';
     } else if (minutes === 30) {
-      return { borderLeft: '1px solid #c3bebe' };
+      style += ' cellBorderHalfHour';
     }
-    return {};
+
+    // if have a user to hightLight and is present at this cell
+    if (heatMapMode) {
+      if (heightlightedUser) {
+        if (_.find(participants, heightlightedUser)) {
+          style += ' cellHighlighted';
+        } else {
+          style += ' cellNotHeiglighted';
+        }
+      }
+    }
+    return style;
+  }
+
+  static formatCellBackgroundColor(heatMapMode, participants, backgroundColors, curUser) {
+    if (heatMapMode) {
+      if (participants.length > 0) {
+        return backgroundColors[participants.length - 1];
+      }
+      return 'transparent';
+    }
+
+    if (_.find(participants, curUser._id)) {
+      return '#000000';
+    }
+    if (participants.length > 0) {
+      return '#DADADA';
+    }
+    return 'transparent';
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      participants: [],
+      heatMapMode: false,
+    };
+  }
+
+  componentWillMount() {
+    const {
+      date, participants, heatMapMode, rowIndex, columnIndex, heightlightedUser } = this.props;
+    this.setState({
+      date: moment(date), participants, heatMapMode, rowIndex, columnIndex, heightlightedUser,
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { date, participants, heatMapMode, heightlightedUser } = nextProps;
+    this.setState({ date: moment(date), participants, heatMapMode, heightlightedUser });
   }
 
   render() {
-    const {
-      time, date, row, col,
-      onMouseDown,
-      onMouseUp,
-      onMouseOver,
-      onMouseLeave,
-    } = this.props;
-    const inlineStyle = this.constructor.formatCellBorder(time);
+    const { date, participants, heatMapMode, heightlightedUser } = this.state;
+    const { backgroundColors, curUser } = this.props;
+    const { formatCellBackgroundColor, styleNameCompose } = this.constructor;
+
+    const styleNames = styleNameCompose(
+      heightlightedUser, heatMapMode, participants, backgroundColors, curUser, date);
+
+    const inlineStyle = {
+      backgroundColor: formatCellBackgroundColor(
+        heatMapMode, participants, backgroundColors, curUser),
+    };
+
     return (
       <div
+        role="presentation"
         style={inlineStyle}
-        data-time={time}
-        data-date={date}
-        data-col={col}
-        data-row={row}
-        onMouseDown={onMouseDown}
-        onMouseUp={onMouseUp}
-        onMouseOver={onMouseOver}
-        onMouseLeave={onMouseLeave}
-        className="cell"
-        styleName="cell"
+        styleName={styleNames}
+        key={date}
+        onMouseOver={this.props.onMouseOver}
+        onMouseLeave={this.props.onMouseLeave}
+        onMouseDown={this.props.onMouseDown}
+        onMouseUp={this.props.onMouseUp}
       />
     );
   }
 }
 
 CellGrid.defaultProps = {
-  isSelected: false,
-  styleName: 'cell',
+  backgroundColors: ['transparent'],
+  rowIndex: 0,
+  columnIndex: 0,
+  heightlightedUser: '',
 };
 
 CellGrid.propTypes = {
-  time: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  row: PropTypes.number.isRequired,
-  col: PropTypes.number.isRequired,
-  onMouseDown: PropTypes.func.isRequired,
-  onMouseUp: PropTypes.func.isRequired,
+  heatMapMode: PropTypes.bool.isRequired,
+  date: PropTypes.instanceOf(Date).isRequired,
+  participants: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.String })).isRequired,
+  backgroundColors: PropTypes.arrayOf(PropTypes.string),
   onMouseOver: PropTypes.func.isRequired,
   onMouseLeave: PropTypes.func.isRequired,
+  onMouseDown: PropTypes.func.isRequired,
+  onMouseUp: PropTypes.func.isRequired,
+  rowIndex: PropTypes.number,
+  columnIndex: PropTypes.number,
+  heightlightedUser: PropTypes.string,
+
+  // Current user
+  curUser: PropTypes.shape({
+    _id: PropTypes.string,      // Unique user id
+    name: PropTypes.string,     // User name
+    avatar: PropTypes.string,   // URL to image representing user(?)
+  }).isRequired,
 };
 
-export default cssModules(CellGrid, styles);
+export default cssModules(CellGrid, styles, { allowMultiple: true });
