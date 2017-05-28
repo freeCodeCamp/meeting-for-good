71812f0bb698b3d9a7351efb9f35d20cffc0fddb
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-05-28 14:06:50 -0300

    add analytics lib

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -132,6 +132,8 @@ module.exports = {
 
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.GoogleAnalyticsID': JSON.stringify(process.env.GoogleAnalyticsID),
+      'process.env.GoogleAnalyticsDebug': JSON.stringify(process.env.GoogleAnalyticsDebug),
     }),
     new ExtractTextPlugin('vendor.css'),
     new OptimizeCSS({
