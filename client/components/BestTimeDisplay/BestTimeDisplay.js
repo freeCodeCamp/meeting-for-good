eb0989ab69c37db0cb5e0a93e1c25f30678efe84
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-06-05 11:29:51 -0300

    clear log

--- client/components/BestTimeDisplay/BestTimeDisplay.js
+++ client/components/BestTimeDisplay/BestTimeDisplay.js
@@ -198,7 +198,6 @@ class BestTimeDisplay extends Component {
             this.constructor.renderRows(displayTimes[date].hours)
           }
         />);
-      console.log(index, quantToShow);
       if (index !== Object.keys(displayTimes).length - 1 && index !== quantToShow - 1) {
         rows.push(
           <Divider key={`Divider ${date}`} styleName="Divider" />,
