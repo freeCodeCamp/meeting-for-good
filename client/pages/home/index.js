81db60b57d3b75a6ea86cc7dc06d3a3ef3a115d1
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-06-14 13:10:53 -0300

    refactor Home

--- client/pages/home/index.js
+++ client/pages/home/index.js
@@ -59,14 +59,8 @@ class Home extends React.Component {
           />
           <hr />
           <h3>The best meeting coordination app</h3>
-          <img
-            src={dashboardBanner}
-            alt="dashboard"
-          />
-          <img
-            src={dashboardBanner2}
-            alt="dashboard2"
-          />
+          <img src={dashboardBanner} alt="dashboard" />
+          <img src={dashboardBanner2} alt="dashboard2" />
         </header>
         <div styleName="content">
           <h3>Easy event creation</h3>
@@ -74,31 +68,21 @@ class Home extends React.Component {
           <img src={mainBannerImage} alt="new event" />
           <hr styleName="hr" />
           <h3>Entering your availability is a breeze</h3>
-          <h6>
-            Using our simplistic availability grid, entering your availability just got easier
-            </h6>
+          <h6> Using our simplistic availability grid, entering your availability just got easier
+          </h6>
           <img src={enterAvailImage} styleName="gif" alt="enter availability" />
           <hr styleName="hr" />
           <h3>Timezones supported</h3>
-          <h6>
-            Don&#39;t worry about having to get everyone to convert to your timezone.
-            Let us take care of it!
-          </h6>
+          <h6>Don&#39;t worry about having to get everyone to convert to your timezone.
+            Let us take care of it!</h6>
           <img src={timeZonesImage} styleName="timezones" alt="timezone" />
-          <p>
-            <small>
-              Same event. Image on the left: UTC+11.
-              Image on the right UTC-5 (Daylight saving included)
-            </small>
-          </p>
+          <p> <small> Same event. Image on the left: UTC+11.
+            Image on the right UTC-5 (Daylight saving included) </small> </p>
         </div>
         <footer>
           <div>
             <div styleName={'divider'} />
-            <p>
-              An Open Source for Good initiative.
-            </p>
-
+            <p> An Open Source for Good initiative. </p>
             <p>
               Check out the repo on <a href="https://github.com/freeCodeCamp/meeting-for-good/" target="_blank" rel="noopener noreferrer">GitHub</a>
             </p>
