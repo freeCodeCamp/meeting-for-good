ec3c6ea024136b5094d03bba57aaafb4abbe0348
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-06-15 19:58:06 -0300

    correct postinstall script

--- tools/postInstall.js
+++ tools/postInstall.js
@@ -1,11 +1,11 @@
 const exec = require('child_process').exec;
 require('dotenv').config();
 
-let args = `-H "Authorization: Bearer${process.env.OP_BEAT_SECRET_TOKEN}" `;
-args += '-d "rev=git log -n 1 --pretty=format:%H" ';
-args += '-d "branch=git rev-parse --abbrev-ref HEAD" ';
-args += '-d status=completed ';
-args += `https://opbeat.com/api/v1/organizations${process.env.OP_BEAT_ORGANIZATION_ID}/apps/${process.env.OP_BEAT_APP_ID}/releases/ `;
+let args = `https://intake.opbeat.com/api/v1/organizations/${process.env.OP_BEAT_ORGANIZATION_ID}/apps/${process.env.OP_BEAT_APP_ID}/releases/ `;
+args += `-H "Authorization: Bearer ${process.env.OP_BEAT_SECRET_TOKEN}" `;
+args += '-d rev=`git log -n 1 --pretty=format:%H` ';
+args += '-d branch=`git rev-parse --abbrev-ref HEAD` ';
+args += '-d status=completed';
 
 console.log(args);
 exec(`curl ${args}`, (error, stdout, stderr) => {
