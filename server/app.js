df192f0bacc0cf51ff3bf62204d5cbcbb39358e3
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-06-21 11:54:30 -0300

    test

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ import routes from './app/routes/routes';
 /* eslint-enable */
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+
 const app = express();
 app.use(compression({ threshold: 0 }));
 mongoose.Promise = bluebird;
