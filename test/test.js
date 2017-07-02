e5bb9692c8078376baa82eb80b818f14a5064f30
Zach <zacharygacer@yahoo.com>
2017-07-02 18:01:30 -0400

    Chaing code to es6 and linting code

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
-var expect  = require('chai').expect;
-var request = require('request');
+import {expect} from 'chai';
+import request from 'request';
 
-describe('Test Server Response', function() {
-    describe ('Server Response', function() {
-        it('status', function(){
-            request('http://localhost:8080/', function(error, response, body) {
-                expect(response.statusCode).to.equal(200);
-            });
-        });
+describe('Test Server Response', () => {
+  describe('Server Response', () => {
+    it('status', () => {
+      request('http://localhost:8080/', (error, response) => {
+        expect(response.statusCode).to.equal(200);
+      });
     });
+  });
 });
