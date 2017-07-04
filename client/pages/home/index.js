242f98b22cf8bba45cb09266aa07936db6492e96
anischyros <anischyros@gmail.com>
2017-07-03 23:07:52 -0700

    Fixed lint complaint.

--- client/pages/home/index.js
+++ client/pages/home/index.js
@@ -15,6 +15,52 @@ import { loadStats } from '../../util/events';
 import LoginModal from '../../components/Login/Login';
 
 class Home extends React.Component {
+  static renderContent() {
+    return (
+      <div styleName="content">
+        <hr />
+        <div>
+          <h3>The best meeting coordination app</h3>
+        </div>
+        <img src={dashboardBanner} alt="dashboard" />
+        <img src={dashboardBanner2} alt="dashboard2" />
+        <hr />
+        <h3>The best meeting coordination app</h3>
+        <img src={dashboardBanner} alt="dashboard" />
+        <img src={dashboardBanner2} alt="dashboard2" />
+        <h3>Easy event creation</h3>
+        <h6>Creating an event is easy as saying 1, 2, 3.</h6>
+        <img src={mainBannerImage} alt="new event" />
+        <hr styleName="hr" />
+        <h3>Entering your availability is a breeze</h3>
+        <h6> Using our simplistic availability grid, entering your availability just got easier
+        </h6>
+        <img src={enterAvailImage} styleName="gifAvail" alt="enter availability" />
+        <hr styleName="hr" />
+        <h3>Timezones supported</h3>
+        <h6>Don&#39;t worry about having to get everyone to convert to your timezone.
+          Let us take care of it!</h6>
+        <img src={timeZonesImage} styleName="timezones" alt="timezone" />
+        <p> <small> Same event. Image on the left: UTC+11.
+          Image on the right UTC-5 (Daylight saving included) </small> </p>
+      </div>
+    );
+  }
+
+  static renderFooter() {
+    return (
+      <footer>
+        <div>
+          <div styleName={'divider'} />
+          <p> An Open Source for Good initiative. </p>
+          <p>
+            Check out the repo on <a href="https://github.com/freeCodeCamp/meeting-for-good/" target="_blank" rel="noopener noreferrer">GitHub</a>
+          </p>
+        </div>
+      </footer>
+    );
+  }
+
   constructor(props) {
     super(props);
 
@@ -96,59 +142,13 @@ class Home extends React.Component {
     );
   }
 
-  renderContent() {
-    return (
-      <div styleName="content">
-        <hr />
-        <div>
-          <h3>The best meeting coordination app</h3>
-        </div>
-        <img src={dashboardBanner} alt="dashboard" />
-        <img src={dashboardBanner2} alt="dashboard2" />
-        <hr />
-        <h3>The best meeting coordination app</h3>
-        <img src={dashboardBanner} alt="dashboard" />
-        <img src={dashboardBanner2} alt="dashboard2" />
-        <h3>Easy event creation</h3>
-        <h6>Creating an event is easy as saying 1, 2, 3.</h6>
-        <img src={mainBannerImage} alt="new event" />
-        <hr styleName="hr" />
-        <h3>Entering your availability is a breeze</h3>
-        <h6> Using our simplistic availability grid, entering your availability just got easier
-        </h6>
-        <img src={enterAvailImage} styleName="gifAvail" alt="enter availability" />
-        <hr styleName="hr" />
-        <h3>Timezones supported</h3>
-        <h6>Don&#39;t worry about having to get everyone to convert to your timezone.
-          Let us take care of it!</h6>
-        <img src={timeZonesImage} styleName="timezones" alt="timezone" />
-        <p> <small> Same event. Image on the left: UTC+11.
-          Image on the right UTC-5 (Daylight saving included) </small> </p>
-      </div>
-    );
-  }
-
-  renderFooter() {
-    return (
-      <footer>
-        <div>
-          <div styleName={'divider'} />
-          <p> An Open Source for Good initiative. </p>
-          <p>
-            Check out the repo on <a href="https://github.com/freeCodeCamp/meeting-for-good/" target="_blank" rel="noopener noreferrer">GitHub</a>
-          </p>
-        </div>
-      </footer>
-    );
-  }
-
   render() {
     const { openLoginModal, loginFail } = this.state;
     return (
       <div styleName="main">
         {this.renderHeader()}
-        {this.renderContent()}
-        {this.renderFooter()}
+        {Home.renderContent()}
+        {Home.renderFooter()}
         <LoginModal
           open={openLoginModal}
           logFail={loginFail}
