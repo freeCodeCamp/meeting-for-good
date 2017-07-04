0f06902e461c57f017e7d98210d9007e35357b7b
anischyros <anischyros@gmail.com>
2017-07-03 23:00:46 -0700

    Split up render() into several functions.

--- client/pages/home/index.js
+++ client/pages/home/index.js
@@ -64,66 +64,91 @@ class Home extends React.Component {
     }
   }
 
-  render() {
-    const { openLoginModal, loginFail, stats } = this.state;
+  renderStats() {
+    const { stats } = this.state;
+    return (
+      <div styleName="statistics">
+        <h6>Event creators: <strong>{stats.users}</strong></h6>
+        <h6>Total events created until today: <strong>{stats.events}</strong></h6>
+        <h6>Events occurring today: <strong>{stats.eventsToday}</strong></h6>
+        <h6>Avg. guests for all events: <strong>{stats.avgParticipants}</strong></h6>
+        <h6>Max. guests for any event: <strong>{stats.maxParticipants}</strong></h6>
+      </div>
+    );
+  }
+
+  renderHeader() {
     const inlineStyle = { loginButton: { textTransform: 'none' } };
     return (
-      <div styleName="main">
-        <header styleName="header">
-          <div styleName="statistics">
-            <h6>Event creators: <strong>{stats.users}</strong></h6>
-            <h6>Total events created until today: <strong>{stats.events}</strong></h6>
-            <h6>Events occurring today: <strong>{stats.eventsToday}</strong></h6>
-            <h6>Avg. guests for all events: <strong>{stats.avgParticipants}</strong></h6>
-            <h6>Max. guests for any event: <strong>{stats.maxParticipants}</strong></h6>
-          </div>
-          <div styleName="title-and-button">
-            <h2>Meeting for Good</h2>
-            <RaisedButton
-              label="Login (it's free!)"
-              styleName="loginButton"
-              labelStyle={inlineStyle.loginButton}
-              onTouchTap={this.handleOpenLoginModal}
-            />
-          </div>
-          <div styleName="dummy-element" />
-        </header>
-        <div styleName="content">
-          <hr />
-          <div>
-            <h3>The best meeting coordination app</h3>
-          </div>
-          <img src={dashboardBanner} alt="dashboard" />
-          <img src={dashboardBanner2} alt="dashboard2" />
-          <hr />
-          <h3>The best meeting coordination app</h3>
-          <img src={dashboardBanner} alt="dashboard" />
-          <img src={dashboardBanner2} alt="dashboard2" />
-          <h3>Easy event creation</h3>
-          <h6>Creating an event is easy as saying 1, 2, 3.</h6>
-          <img src={mainBannerImage} alt="new event" />
-          <hr styleName="hr" />
-          <h3>Entering your availability is a breeze</h3>
-          <h6> Using our simplistic availability grid, entering your availability just got easier
-          </h6>
-          <img src={enterAvailImage} styleName="gifAvail" alt="enter availability" />
-          <hr styleName="hr" />
-          <h3>Timezones supported</h3>
-          <h6>Don&#39;t worry about having to get everyone to convert to your timezone.
-            Let us take care of it!</h6>
-          <img src={timeZonesImage} styleName="timezones" alt="timezone" />
-          <p> <small> Same event. Image on the left: UTC+11.
-            Image on the right UTC-5 (Daylight saving included) </small> </p>
+      <header styleName="header">
+        {this.renderStats()}
+        <div styleName="title-and-button">
+          <h2>Meeting for Good</h2>
+          <RaisedButton
+            label="Login (it's free!)"
+            styleName="loginButton"
+            labelStyle={inlineStyle.loginButton}
+            onTouchTap={this.handleOpenLoginModal}
+          />
         </div>
-        <footer>
-          <div>
-            <div styleName={'divider'} />
-            <p> An Open Source for Good initiative. </p>
-            <p>
-              Check out the repo on <a href="https://github.com/freeCodeCamp/meeting-for-good/" target="_blank" rel="noopener noreferrer">GitHub</a>
-            </p>
-          </div>
-        </footer>
+        <div styleName="dummy-element" />
+      </header>
+    );
+  }
+
+  renderContent() {
+    return (
+      <div styleName="content">
+        <hr />
+        <div>
+          <h3>The best meeting coordination app</h3>
+        </div>
+        <img src={dashboardBanner} alt="dashboard" />
+        <img src={dashboardBanner2} alt="dashboard2" />
+        <hr />
+        <h3>The best meeting coordination app</h3>
+        <img src={dashboardBanner} alt="dashboard" />
+        <img src={dashboardBanner2} alt="dashboard2" />
+        <h3>Easy event creation</h3>
+        <h6>Creating an event is easy as saying 1, 2, 3.</h6>
+        <img src={mainBannerImage} alt="new event" />
+        <hr styleName="hr" />
+        <h3>Entering your availability is a breeze</h3>
+        <h6> Using our simplistic availability grid, entering your availability just got easier
+        </h6>
+        <img src={enterAvailImage} styleName="gifAvail" alt="enter availability" />
+        <hr styleName="hr" />
+        <h3>Timezones supported</h3>
+        <h6>Don&#39;t worry about having to get everyone to convert to your timezone.
+          Let us take care of it!</h6>
+        <img src={timeZonesImage} styleName="timezones" alt="timezone" />
+        <p> <small> Same event. Image on the left: UTC+11.
+          Image on the right UTC-5 (Daylight saving included) </small> </p>
+      </div>
+    );
+  }
+
+  renderFooter() {
+    return (
+      <footer>
+        <div>
+          <div styleName={'divider'} />
+          <p> An Open Source for Good initiative. </p>
+          <p>
+            Check out the repo on <a href="https://github.com/freeCodeCamp/meeting-for-good/" target="_blank" rel="noopener noreferrer">GitHub</a>
+          </p>
+        </div>
+      </footer>
+    );
+  }
+
+  render() {
+    const { openLoginModal, loginFail } = this.state;
+    return (
+      <div styleName="main">
+        {this.renderHeader()}
+        {this.renderContent()}
+        {this.renderFooter()}
         <LoginModal
           open={openLoginModal}
           logFail={loginFail}
