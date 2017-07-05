a7b0cf4204a15d6207d329f3de9d22388978f67c
Zach <zacharygacer@yahoo.com>
2017-07-05 13:30:18 -0400

    Modified testing setup

--- test/test.js
+++ test/test.js
@@ -1,6 +1,17 @@
-import { chai, should } from 'chai';
+import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../server/app';
+const should = chai.should()
 
 chai.use(chaiHttp);
 
+describe('Test user API', () => {
+  it('it should authenticate', (done) => {
+    chai.request(server)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+  });
+});
