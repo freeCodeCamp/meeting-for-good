c4986a5ed5bb09af3af41c473d7d5051d460f0a7
Zach <zacharygacer@yahoo.com>
2017-07-05 19:26:21 -0400

    Testing for 403 error when faield to authenticate

--- test/test.js
+++ test/test.js
@@ -1,16 +1,21 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../server/app';
-const should = chai.should()
+const should = chai.should();
 
 chai.use(chaiHttp);
-
+/*
+import mongoose from 'mongoose';
+import User from '../server/api/user/user.controller'
+*/
 describe('Test user API', () => {
   it('it should authenticate', (done) => {
     chai.request(server)
-        .get('/')
+        .get('/api/user')
         .end((err, res) => {
-          res.should.have.status(200);
+          //console.log("The res body is "+ res.body)
+          res.should.have.status(403);
+          //assert.equal(res.body.isAuthenticated, 'Authentiation required.');
           done();
         });
   });
