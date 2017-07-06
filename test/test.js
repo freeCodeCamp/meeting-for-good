bd5b9bee84a70ed06e5ab1a9e24f1f5eeb495507
Zach <zacharygacer@yahoo.com>
2017-07-05 20:51:57 -0400

    Adding node test enviroment

--- test/test.js
+++ test/test.js
@@ -1,21 +1,20 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../server/app';
-const should = chai.should();
+let should = chai.should();
+let assert = chai.assert;
 
 chai.use(chaiHttp);
-/*
+
 import mongoose from 'mongoose';
 import User from '../server/api/user/user.controller'
-*/
+
 describe('Test user API', () => {
-  it('it should authenticate', (done) => {
+  it('Testing Failed Authentication', (done) => {
     chai.request(server)
         .get('/api/user')
         .end((err, res) => {
-          //console.log("The res body is "+ res.body)
           res.should.have.status(403);
-          //assert.equal(res.body.isAuthenticated, 'Authentiation required.');
           done();
         });
   });
