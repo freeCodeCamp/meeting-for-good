4e02d311e70b4dddb5204a4f34f379990faffd3f
Zach <zacharygacer@yahoo.com>
2017-07-07 07:20:19 -0400

    Adding a conditional to isAuth controller during testing. Then, deleting afterEach and post test.

--- test/test.js
+++ test/test.js
@@ -1,20 +1,32 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../server/app';
-let should = chai.should();
-let assert = chai.assert;
+
+import User from '../server/api/user/user.model';
+
+const should = chai.should();
 
 chai.use(chaiHttp);
 
-import mongoose from 'mongoose';
-import User from '../server/api/user/user.controller'
-
 describe('Test user API', () => {
-  it('Testing Failed Authentication', (done) => {
+  beforeEach((done) => {
+    const newUser = new User({
+      googleId: 'googletest@email',
+      facebookId: 'facebooktest@email',
+      emails: ['email1@email.com', 'email2@email'],
+      name: 'name',
+      avatar: 'avatar',
+      accessToken: '12345',
+    });
+    newUser.save(() => {
+      done();
+    });
+  });
+  it('should get a status of 200. Testing GET /api/user', (done) => {
     chai.request(server)
-        .get('/api/user')
+        .get('/api/user') 
         .end((err, res) => {
-          res.should.have.status(403);
+          res.should.have.status(200);
           done();
         });
   });
