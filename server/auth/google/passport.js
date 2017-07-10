8f8d0176362e9ada85073811284860f0e97062de
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-07-10 20:37:42 -0300

    enahcne manipulateUser

--- server/auth/google/passport.js
+++ server/auth/google/passport.js
@@ -3,12 +3,12 @@ import refresh from 'passport-oauth2-refresh';
 
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-const manipulateUser = async (User, profile, done, token, refreshToken) => {
+const manipulateUser = async (User, profile, done) => {
   try {
     const user = await User.findOne({ googleId: profile.id });
     if (user) {
-      user.accessToken = token;
-      user.refreshToken = refreshToken;
+      user.accessToken = profile.token;
+      user.refreshToken = profile.refreshToken;
       await user.save();
       return done(null, user);
     }
@@ -16,8 +16,8 @@ const manipulateUser = async (User, profile, done, token, refreshToken) => {
     newUser.googleId = profile.id;
     newUser.name = profile.displayName;
     newUser.avatar = profile.photos[0].value;
-    newUser.accessToken = token;
-    newUser.refreshToken = refreshToken;
+    newUser.accessToken = profile.token;
+    newUser.refreshToken = profile.refreshToken;
     profile.emails.forEach((email) => { newUser.emails.push(email.value); });
     await newUser.save();
     return done(null, newUser);
@@ -31,8 +31,11 @@ const strategy = (User, config) => new GoogleStrategy({
   clientID: config.googleAuth.clientID,
   clientSecret: config.googleAuth.clientSecret,
   callbackURL: config.googleAuth.callbackURL,
-}, async (token, refreshToken, profile, done) =>
-    manipulateUser(User, profile, done, token, refreshToken));
+}, async (token, refreshToken, profile, done) => {
+  profile.token = token;
+  profile.refreshToken = refreshToken;
+  return manipulateUser(User, profile, done);
+});
 
 export const setup = (User, config) => {
   passport.use('google', strategy(User, config));
