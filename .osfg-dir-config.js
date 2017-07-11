2f31e9439197d57bbb6bac7c94f5d1437d9d2178
Michael D. Johnson <michael@freecodecamp.com>
2017-07-11 19:01:30 -0400

    Create .osfg-dir-config.js

---
+++ .osfg-dir-config.js
@@ -0,0 +1,27 @@
+/*
+  OPEN SOURCE FOR GOOD DIRECTORY SAMPLE CONFIG FILE 
+
+  title: Displayed at the Top and as the WebPage Title
+  demoVideo: for the moment it only works with a regular YouTube links.
+  liveDemo: address to the Homepage of the Project.
+  description: an ES6 Template String with Github Markdown.
+               Keep it relatively short, a paragraph tops.
+  body: an ES6 Template String with Github Markdown. 
+        Please include the license and FreeCodeCamp copyright at the end.
+
+  If the 'liveDemo' or the 'demoVideo' aren't yet available you can 
+  exclued them, they just won't be added to the Project's page.
+*/
+
+module.exports = {
+  title: 'Meeting for Good',
+  demoVideo: 'https://www.youtube.com/watch?v=LVZbUDzx1a0&',
+  liveDemo: 'https://meeting.freecodecamp.com/',
+  description: `A tool to effortlessly schedule calls and meetings across multiple timezones.',
+  body: `#### License
+
+This computer software is licensed under the Open Source [BSD-3-Clause](https://github.com/freeCodeCamp/Mail-for-Good/blob/master/LICENSE.md).
+
+Copyright (c) 2017, freeCodeCamp.
+`
+};
